e650f496af01a28e9dcf652e73be13bdd4b652e7
Huong-hihi <dothihuong030499@gmail.com>
2021-04-21 15:59:10 +0700

    ok

---
+++ resources/views/about.blade.php
@@ -0,0 +1,47 @@
+@extends('exam1.default')
+
+@section('title', 'contact')
+
+@section('content')
+    <div class="page-about about_area bg--white section-padding--lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section__title--3 text-center pb--30">
+                        <h2>Thông Tin Nhà Phát Triển</h2>
+                    </div>
+                </div>
+            </div>
+    <!-- End About Area -->
+    <!-- Start Team Area -->
+    <section class="wn__team__area pb--75 bg--white">
+        <div class="col-lg-12">
+        <div class="row">
+                <!-- End Single Team -->
+                <!-- Start Single Team -->
+                <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                    <div class="wn__team">
+                        <div class="thumb">
+                            <img src="img/about/huong.jpg" alt="images">
+                        </div>
+                        <div class="content text-center">
+                            <h4>ĐỖ THỊ HƯƠNG</h4>
+                            <p>hedspi</p>
+                            <ul class="team__social__net">
+                                <li><a href="https://www.facebook.com/huong34.hihi/"><i class="icon-social-facebook icons"></i></a></li>
+                                <li><a href="https://www.instagram.com/__huonghihi34/?hl=vi"><i class="icon-social-linkedin icons"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-12"> 
+        <h4>hihiihhhhhhhhhhhhhhhhhhh</h4>
+        </div>
+                </div>
+               
+                <!-- End Single Team -->
+            </div>
+        </div>
+        </div>
+       
+    </section>
+@endsection
